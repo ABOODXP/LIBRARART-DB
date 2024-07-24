d6f3a76ae68feda5212ce939a4748f2b5f077943
ABOODXP <a.y.aodah3412@gmail.com>
2024-07-24 21:29:48 +0300

    LIBRARY DB SYSTEM

---
+++ LIBRARY DB SYSTEM/Form1.Designer.cs
@@ -0,0 +1,316 @@
+﻿namespace LIBRARY_DB_SYSTEM
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.tableBookdbBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.lIBRARY_DBDataSet = new LIBRARY_DB_SYSTEM.LIBRARY_DBDataSet();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.table_bookdbTableAdapter = new LIBRARY_DB_SYSTEM.LIBRARY_DBDataSetTableAdapters.Table_bookdbTableAdapter();
+            this.button5 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tableBookdbBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.lIBRARY_DBDataSet)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Green;
+            this.button1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button1.Location = new System.Drawing.Point(68, 431);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(129, 46);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "INSERT";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Red;
+            this.button2.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button2.Location = new System.Drawing.Point(335, 431);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(124, 46);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "DELETE";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.DarkSalmon;
+            this.button3.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button3.Location = new System.Drawing.Point(620, 431);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(124, 46);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "UPDATE";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.SystemColors.Highlight;
+            this.button4.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button4.Location = new System.Drawing.Point(1166, 431);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(124, 46);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "CLOSE";
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Tahoma", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(42, 78);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(108, 27);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "ID BOOK:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Tahoma", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(685, 82);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(142, 27);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "NAME BOOK:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Tahoma", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(42, 173);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(166, 27);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "AUTHER BOOK:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Tahoma", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(42, 272);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(142, 27);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "TITLE BOOK:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Tahoma", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(685, 173);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(133, 27);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "ISBN BOOK:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Tahoma", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(685, 279);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(146, 27);
+            this.label6.TabIndex = 9;
+            this.label6.Text = "SHELF BOOK:";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(164, 502);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 62;
+            this.dataGridView1.RowTemplate.Height = 29;
+            this.dataGridView1.Size = new System.Drawing.Size(996, 372);
+            this.dataGridView1.TabIndex = 10;
+            // 
+            // textBox1
+            // 
+            this.textBox1.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.tableBookdbBindingSource, "id", true));
+            this.textBox1.Font = new System.Drawing.Font("Tahoma", 11F);
+            this.textBox1.Location = new System.Drawing.Point(214, 75);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(301, 34);
+            this.textBox1.TabIndex = 11;
+            // 
+            // tableBookdbBindingSource
+            // 
+            this.tableBookdbBindingSource.DataMember = "Table bookdb";
+            this.tableBookdbBindingSource.DataSource = this.lIBRARY_DBDataSet;
+            // 
+            // lIBRARY_DBDataSet
+            // 
+            this.lIBRARY_DBDataSet.DataSetName = "LIBRARY_DBDataSet";
+            this.lIBRARY_DBDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // textBox2
+            // 
+            this.textBox2.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.tableBookdbBindingSource, "name", true));
+            this.textBox2.Font = new System.Drawing.Font("Tahoma", 11F);
+            this.textBox2.Location = new System.Drawing.Point(858, 75);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(362, 34);
+            this.textBox2.TabIndex = 12;
+            // 
+            // textBox3
+            // 
+            this.textBox3.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.tableBookdbBindingSource, "auther", true));
+            this.textBox3.Font = new System.Drawing.Font("Tahoma", 11F);
+            this.textBox3.Location = new System.Drawing.Point(214, 166);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(301, 34);
+            this.textBox3.TabIndex = 13;
+            // 
+            // textBox4
+            // 
+            this.textBox4.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.tableBookdbBindingSource, "title", true));
+            this.textBox4.Font = new System.Drawing.Font("Tahoma", 11F);
+            this.textBox4.Location = new System.Drawing.Point(214, 269);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(301, 34);
+            this.textBox4.TabIndex = 14;
+            // 
+            // textBox5
+            // 
+            this.textBox5.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.tableBookdbBindingSource, "ISBN", true));
+            this.textBox5.Font = new System.Drawing.Font("Tahoma", 11F);
+            this.textBox5.Location = new System.Drawing.Point(858, 166);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(362, 34);
+            this.textBox5.TabIndex = 15;
+            // 
+            // textBox6
+            // 
+            this.textBox6.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.tableBookdbBindingSource, "sh book", true));
+            this.textBox6.Font = new System.Drawing.Font("Tahoma", 11F);
+            this.textBox6.Location = new System.Drawing.Point(858, 279);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(362, 34);
+            this.textBox6.TabIndex = 16;
+            // 
+            // table_bookdbTableAdapter
+            // 
+            this.table_bookdbTableAdapter.ClearBeforeFill = true;
+            // 
+            // button5
+            // 
+            this.button5.BackColor = System.Drawing.Color.LightSeaGreen;
+            this.button5.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button5.Location = new System.Drawing.Point(903, 431);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(124, 46);
+            this.button5.TabIndex = 17;
+            this.button5.Text = "DISPLAY";
+            this.button5.UseVisualStyleBackColor = false;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 19F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.BurlyWood;
+            this.ClientSize = new System.Drawing.Size(1347, 948);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tableBookdbBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.lIBRARY_DBDataSet)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox6;
+        private LIBRARY_DBDataSet lIBRARY_DBDataSet;
+        private System.Windows.Forms.BindingSource tableBookdbBindingSource;
+        private LIBRARY_DBDataSetTableAdapters.Table_bookdbTableAdapter table_bookdbTableAdapter;
+        private System.Windows.Forms.Button button5;
+    }
+}
+
---
+++ LIBRARY DB SYSTEM/Form1.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace LIBRARY_DB_SYSTEM
+{
+    public partial class Form1 : Form
+    {
+        SqlConnection con = new SqlConnection("Data Source=LAPTOP-168VT5JN\\SQLEXPRESS;Initial Catalog=\"LIBRARY DB\";Integrated Security=True");
+        //private const string connectionString = "Data Source=LAPTOP-168VT5JN\\SQLEXPRESS;Initial Catalog=\"LIBRARY DB\";Integrated Security=True";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'lIBRARY_DBDataSet.Table_bookdb' table. You can move, or remove it, as needed.
+            this.table_bookdbTableAdapter.Fill(this.lIBRARY_DBDataSet.Table_bookdb);
+         
+        }
+
+        
+        private void button1_Click(object sender, EventArgs e)
+        {
+            con.Open();
+            SqlCommand cmd = con.CreateCommand();
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = "insert into [Table bookdb] (id,name,title,auther,ISBN,[sh book]) values ('"+textBox1.Text+"','"+textBox2.Text+"','"+textBox4.Text+"','"+textBox3.Text+"','"+textBox5.Text+"','"+textBox6.Text+"')";
+            cmd.ExecuteNonQuery();
+            con.Close();
+            textBox1.Text = "";
+            textBox2.Text = "";
+            textBox4.Text = "";
+            textBox3.Text = "";
+            textBox5.Text = "";
+            textBox6.Text = "";
+            display_data();
+            MessageBox.Show("succesully inserted!");
+        }
+
+        public void display_data()
+        {
+            con.Open();
+            SqlCommand cmd = con.CreateCommand();
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = "select * from [Table bookdb]";
+            cmd.ExecuteNonQuery();
+            DataTable dta = new DataTable();
+            SqlDataAdapter dataapd = new SqlDataAdapter(cmd);
+            dataapd.Fill(dta);
+            dataGridView1.DataSource = dta;
+            con.Close();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            con.Open();
+            SqlCommand cmd = con.CreateCommand();
+            cmd.CommandType = CommandType.Text;
+            //cmd.CommandText = "delete from [Table bookdb] where name = '" + textBox2.Text + "'";
+            //cmd.CommandText = "delete from [Table bookdb] where id = '" + textBox1.Text + "' AND name = '" + textBox2.Text + "' AND title = '" + textBox4.Text + "' AND auther = '" + textBox3.Text + "' AND ISBN = '" + textBox5.Text + "' AND sh book = '" + textBox6.Text + "'";
+            cmd.CommandText = "delete from [Table bookdb] where id = @id AND name = @name AND title = @title AND auther = @author AND ISBN = @isbn AND [sh book] = @shBook";
+            cmd.Parameters.AddWithValue("@id", textBox1.Text);
+            cmd.Parameters.AddWithValue("@name", textBox2.Text);
+            cmd.Parameters.AddWithValue("@title", textBox4.Text);
+            cmd.Parameters.AddWithValue("@author", textBox3.Text);
+            cmd.Parameters.AddWithValue("@isbn", textBox5.Text);
+            cmd.Parameters.AddWithValue("@shBook", textBox6.Text);
+            cmd.ExecuteNonQuery();
+            con.Close();
+            textBox1.Text = "";
+            textBox2.Text = "";
+            textBox4.Text = "";
+            textBox3.Text = "";
+            textBox5.Text = "";
+            textBox6.Text = "";
+            display_data();
+            MessageBox.Show("succesully deleted!");
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            display_data();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            con.Open();
+            SqlCommand cmd = con.CreateCommand();
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = "update [Table bookdb] set name = '" + textBox2.Text + "' where name = '"+textBox2.Text+"'";
+            cmd.ExecuteNonQuery();
+            con.Close();
+            textBox1.Text = "";
+            textBox2.Text = "";
+            textBox4.Text = "";
+            textBox3.Text = "";
+            textBox5.Text = "";
+            textBox6.Text = "";
+            display_data();
+            MessageBox.Show("succesully updated!");
+        }
+    }
+}
